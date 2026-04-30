2cfe287ba38b701dd250056b1e4f3d780e191e7c
treeso47 <treeso47@yahoo.com>
2026-04-30 17:38:28 +0000

    add way to keep track of time and snake length

--- snake.c
+++ snake.c
@@ -1,6 +1,6 @@
 #include <ncurses.h>
 #include <stdlib.h>
-//#include <time.h>
+#include <time.h>
 #include <unistd.h>     //only here for testing basic layout with sleep()
 
 // Directions
@@ -19,6 +19,16 @@ int direction = RIGHT;
 int foodX, foodY;
 int score = 0;
 int maxX, maxY;
+int snakeLength, goalLength;    //to keep track of current and goal lengths
+
+//function for getting time played
+
+int time_update(time_t start){
+    time_t current = time(NULL);
+    int elapsed = difftime(current, start);
+
+    return elapsed;
+}
 
 int main(){
     initscr();
@@ -30,18 +40,26 @@ int main(){
     //refresh();
 
     //Might remove later, define box
-    WINDOW *a = newwin(LINES-1,COLS,1,0);
+    WINDOW *gameWin = newwin(LINES-1,COLS,1,0);
     refresh();
-    box(a,0,0);
-    wrefresh(a);
-    nodelay(a, TRUE);
-    getmaxyx(a, maxY, maxX);
+    box(gameWin,0,0);
+    wrefresh(gameWin);
+    nodelay(gameWin, TRUE);
+    getmaxyx(gameWin, maxY, maxX);
     refresh();
+
+    
+    snakeLength = 3;
+    goalLength = 2*(maxY+maxX);
+    
+    //record when started
+    time_t start = time(NULL);
 
     while(1){
         // Display score
         mvprintw(0, 2, "Score: %d", score);
-        //mvprintw(maxY/2, (maxX/2)-9, "height: %d width: %d", maxX, maxY);  //testing for issues with maxX & maxY
+        int cur_time = time_update(start);
+        mvprintw(0, COLS - 6, "%2d:%02d", cur_time/60, cur_time%60);
         refresh();
 
         // checking text input locations, looks like must be before if for input
@@ -57,19 +75,20 @@ int main(){
         else if ((input == 'a' || input == KEY_LEFT) && direction != RIGHT) direction = LEFT;
         else if ((input == 'd' || input == KEY_RIGHT) && direction != LEFT) direction = RIGHT;
         else if (input == 'q' || input == 'Q') break;
-    } 
+    }
 
     //sleep(1);
     // Game over screen
-    clear();
-    refresh();
-    box(a,0,0);
-    mvprintw(maxY / 2, (maxX / 2) - 5, "GAME OVER");
-    mvprintw(maxY / 2 + 1, (maxX / 2) - 7, "Final Score: %d", score);
-    // mvwprintw(a, LINES / 2 - 1, (COLS / 2) - 5, "GAME OVER");       //temporary solution
-    // mvwprintw(a, LINES / 2, (COLS / 2) - 7, "Final Score: %d", score);
+    wclear(gameWin);
+    wrefresh(gameWin);
+    box(gameWin,0,0);
+    // mvprintw(maxY / 2, (maxX / 2) - 5, "GAME OVER");
+    // mvprintw(maxY / 2 + 1, (maxX / 2) - 7, "Final Score: %d", score);
+    if(snakeLength == goalLength){mvprintw(maxY / 2, (maxX / 2) - 4, "You Win");}
+    else{mvprintw(maxY / 2, (maxX / 2) - 5, "GAME OVER");}
+    mvwprintw(gameWin, LINES / 2, (COLS / 2) - 7, "Final Score: %d", score);
     //mvprintw(LINES / 2, (COLS / 2) - 5, "GAME OVER");
-    wrefresh(a);
+    wrefresh(gameWin);
     refresh();
     nodelay(stdscr, FALSE);
     getch();        //wait for user input before closing
